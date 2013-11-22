8ff6ede76d23b5a2af6fdf7e6e278fd8d2051e6a
Marc Weber <marco-oweber@gmx.de>
2013-11-22 02:13:44 +0100

    doh

--- lib/mw/assertions.rb
+++ lib/mw/assertions.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
-class Nil
+class NilClass
   def assert_nil; self; end
   def assert_not_nil; raise "non nil value expected"; end
 end
@@ -9,11 +9,14 @@ class Symbol
   def assert_sym; self; end
 end
 
-class Object
+class BasicObject
   def assert_nil; raise "nil expected"; end
   def assert_not_nil; self; end
-  def assert_sym; raise "symbol expected"; end
+  def assert_sym; raise "symbol expected, but got #{self}"; end
   def assert_instance_of(clazz); raise "instance of #{clazz} expected" unless self.instance_of? clazz; self; end
+  def assert_condition(&blk)
+    raise "condition failed" unless blk.call(self)
+  end
 end
 
 class Hash
